9c6819f3c3c3ed3494d5c9923463d28d75eca7de
Curious Cat <35210356+only4sim@users.noreply.github.com>
2023-10-26 21:55:36 +0000

    Add python to o1js

--- simple_python_to_o1js.py
+++ simple_python_to_o1js.py
@@ -1,19 +1,40 @@
-translations = {
-    "field ": "new Field(",  # Translates a field declaration
-    " = ": ").add(",         # Translates an assignment and addition
-    ";\n": ")\n",           # Closes the Field declaration
-}
+import ast
+import astor
 
-def translate_to_o1js(code, translations):
-    for key, value in translations.items():
-        code = code.replace(key, value)
-    return code
+class PythonToO1JS(ast.NodeTransformer):
+    def visit_BinOp(self, node):
+        if isinstance(node.op, ast.Add):
+            new_node = ast.Call(
+                func=ast.Attribute(
+                    value=ast.Call(
+                        func=ast.Name(id='Field', ctx=ast.Load()),
+                        args=[self.generic_visit(node.left)],
+                        keywords=[]
+                    ),
+                    attr='add',
+                    ctx=ast.Load()
+                ),
+                args=[self.generic_visit(node.right)],
+                keywords=[]
+            )
+            return ast.Call(
+                func=ast.Name(id='Field', ctx=ast.Load()),
+                args=[new_node],
+                keywords=[]
+            )
+        return node
 
-source_code = """
-field a = 5;
-field b = 10;
-field result = a + b;
+    def visit_Num(self, node):
+        return ast.Str(s=str(node.n))
+
+def translate_to_o1js(python_code):
+    tree = ast.parse(python_code)
+    transformer = PythonToO1JS()
+    new_tree = transformer.visit(tree)
+    return astor.to_source(new_tree)
+
+python_code = """
+result = 5 + 3
 """
-
-translated_code = translate_to_o1js(source_code, translations)
-print(translated_code)
+o1js_code = translate_to_o1js(python_code)
+print(o1js_code)
