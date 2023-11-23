73207bd5b961c1325ff5c802de64f680221d2384
only4sim <vquanli@126.com>
2023-11-23 21:25:03 +0100

    Update tested argMax

--- src/libs/argMax.ts
+++ src/libs/argMax.ts
@@ -1,13 +1,39 @@
-function argMax(arr) {
-    let maxIndex = 0;
-    let maxValue = arr[0];
+import {
+    Field,
+    Mina,
+    PrivateKey,
+    AccountUpdate,
+    SelfProof,
+    ZkProgram,
+    Struct,
+    Bool,
+    Circuit,
+    Poseidon,
+    MerkleMap,
+    MerkleTree,
+    MerkleWitness,
+    MerkleMapWitness,
+    verify,
+    Provable,
+  } from 'o1js';
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i].greaterThan(maxValue)) {
-            maxIndex = i;
-            maxValue = arr[i];
-        }
-    }
 
-    return maxIndex;
-}
+export const ArgMax = ZkProgram({
+    name: 'ArgMax',
+    publicOutput: Field,
+  methods: {
+    argMax: {
+      privateInputs: [Provable.Array(Field, 5)],
+
+      method(arr: Field[]): Field {
+        let maxIndex = Field(0);
+        let maxVal = arr[0];
+        for (let i=1; i<arr.length; i++) {
+            maxIndex = Provable.if(maxVal.greaterThan(arr[i]), maxIndex, Field(i));
+            maxVal = Provable.if(maxVal.greaterThan(arr[i]), maxVal, arr[i]);
+      };
+        return maxIndex;
+    },
+  },
+},
+});
