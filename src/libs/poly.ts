7dd97d3b6bb19ae3adceccc0abe0e288aa588256
only4sim <vquanli@126.com>
2023-11-26 23:47:27 +0100

    Update zkprogram poly

--- src/libs/poly.ts
+++ src/libs/poly.ts
@@ -1,10 +1,41 @@
-function poly(input, coefficients) {
-    let result = new Field(0);
+import {
+    Field,
+    Mina,
+    PrivateKey,
+    AccountUpdate,
+    SelfProof,
+    ZkProgram,
+    Struct,
+    Bool,
+    Circuit,
+    Poseidon,
+    MerkleMap,
+    MerkleTree,
+    MerkleWitness,
+    MerkleMapWitness,
+    verify,
+    Provable,
+  } from 'o1js';
 
-    for (let i = 0; i < coefficients.length; i++) {
-        const term = coefficients[i].mul(input.pow(i));
-        result = result.add(term);
-    }
+const Poly = ZkProgram({
+    name: 'Poly',
+    publicOutput: Field,
+  methods: {
+    poly: {
+      privateInputs: [Field, Provable.Array(Field, 5)],
 
-    return result;
-}
+      method(input: Field, coefficients: Field[]): Field {
+        let result = new Field(0);
+        let power = new Field(1);
+
+        for (let i = 0; i < coefficients.length; i++) {
+            const term = coefficients[i].mul(power);
+            result = result.add(term);
+            power = power.mul(input);
+        }  
+        return result;
+    },
+  },
+},
+});
+
